8bc0d421e8a211e0208d5ae650242110a74aae22
Harri Hämäläinen <hhamalai@iki.fi>
2014-02-06 12:23:32 +0200

    migrate the project to Github

---
+++ setup.py
@@ -0,0 +1,10 @@
+import subprocess
+from setuptools import setup, find_packages, Extension
+
+setup(
+ name='telemetryfdw',
+ version='0.0.1',
+ author='Harri Hämäläinen',
+ license='Postgresql',
+ packages=['telemetryfdw']
+)
